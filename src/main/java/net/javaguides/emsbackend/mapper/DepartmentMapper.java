312fb98192236ea8891836fd7715c815cba8eca3
inhtruong <duydat.dhsp@gmail.com>
2025-03-08 20:20:37 +0700

    Create Department DTO and Mapper

---
+++ src/main/java/net/javaguides/emsbackend/mapper/DepartmentMapper.java
@@ -0,0 +1,24 @@
+package net.javaguides.emsbackend.mapper;
+
+import net.javaguides.emsbackend.dto.DepartmentDto;
+import net.javaguides.emsbackend.entities.Department;
+
+public class DepartmentMapper {
+    // Convert department jpa entity to deparment dto
+    public static DepartmentDto mapToDepartmentDto(Department department) {
+        return new DepartmentDto(
+                department.getId(),
+                department.getDepartmentName(),
+                department.getDepartmentDescription()
+        );
+    }
+
+    // Convert department dto to department jpa entity
+    public static Department mapToDepartment(DepartmentDto departmentDto) {
+        return new Department(
+                departmentDto.getId(),
+                departmentDto.getDepartmentName(),
+                departmentDto.getDepartmentDescription()
+        );
+    }
+}
